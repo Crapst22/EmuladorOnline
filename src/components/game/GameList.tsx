5612781ff51da0a94426e79e0c96b958cb81beca
Valentino <valentino.pajon@gmail.com>
2026-06-09 17:45:21 -0300

    fix barra de busqueda

--- src/components/game/GameList.tsx
+++ src/components/game/GameList.tsx
@@ -75,7 +75,7 @@ export function GameList() {
       <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
         <div className="relative flex-1 max-w-md">
           <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-[#FFD700]/40" />
-          <Input placeholder="BUSCAR JUEGOS..." className="pl-10" value={search} onChange={(e) => setSearch(e.target.value)} />
+          <Input placeholder="BUSCAR JUEGOS..." className="!pl-12" value={search} onChange={(e) => setSearch(e.target.value)} />
         </div>
         <UploadRom onUploadComplete={loadGames} />
       </div>
